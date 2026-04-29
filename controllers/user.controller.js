62fab6b12c58800b46e546055dc7503e267726af
smfahim25 <smfahimhossen@gmail.com>
2026-04-29 11:24:44 +0600

    update code

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -251,7 +251,7 @@ exports.faceVerify = async (req, res) => {
     if (!req.file) return res.status(400).json({ error: "No image uploaded" });
 
     await User.update(user_id, { face_image: req.file ? req.file.path : null });
-    res.json({ message: "Face image uploaded successfully" });
+    res.json({ message: "Face image uploaded successfully." });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
